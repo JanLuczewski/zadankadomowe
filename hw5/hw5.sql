f50c2dca9e87c34cfd704f2d7488c2ddfa31aecf
JanLuczewski <jan.luczewski@gmail.com>
2022-02-08 19:28:10 +0100

    cos tam cos tam

---
+++ hw5/hw5.sql
@@ -0,0 +1,29 @@
+--HW5
+--Stworzyć bazę danych i tabele: uzytkownik, zamówienie i produkt. Zapisać SQLową komendę (CREATE DATABASE ... + CREATE TABLE)
+--Tabela zamówienia powinna mieć relację do użytkownika i do produktu (FOREIGN KEY)
+--Napisać komendy do dodania kilku przykładowych zamówień, użytkowników i produktów
+CREATE DATABASE fakturki;
+CREATE TABLE zamowienie (
+    id int NOT NULL AUTO_INCREMENT,
+    uzytkownik_id int,
+    produkt_id int,
+    ile int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (uzytkownik_id) REFERENCES uzytkownik(id),
+    FOREIGN KEY (produkt_id) REFERENCES produkt(id)
+
+)
+
+CREATE TABLE uzytkownik (
+    id int NOT NULL AUTO_INCREMENT,
+    imie varchar(255),
+    nazwisko varchar(255),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE produkt (
+    id int NOT NULL AUTO_INCREMENT,
+    nazwa varchar(255),
+    kategoria varchar(255),
+    PRIMARY KEY (id)
+)
