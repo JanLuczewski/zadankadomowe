6d0c6f32ec8aa93d09ace4d60fb6b3ec982219da
JanLuczewski <jan.luczewski@gmail.com>
2022-02-09 08:31:23 +0100

    zadanko 5 zrobione

--- hw5/hw5.sql
+++ hw5/hw5.sql
@@ -11,19 +11,24 @@ CREATE TABLE zamowienie (
     PRIMARY KEY (id),
     FOREIGN KEY (uzytkownik_id) REFERENCES uzytkownik(id),
     FOREIGN KEY (produkt_id) REFERENCES produkt(id)
-
-)
+);
+INSERT INTO zamowienie
+VALUES (1, 1, 9);
 
 CREATE TABLE uzytkownik (
     id int NOT NULL AUTO_INCREMENT,
     imie varchar(255),
     nazwisko varchar(255),
     PRIMARY KEY (id)
-)
+);
+INSERT INTO uzytkownik
+VALUES ('Karol','Mularski');
 
 CREATE TABLE produkt (
     id int NOT NULL AUTO_INCREMENT,
     nazwa varchar(255),
     kategoria varchar(255),
     PRIMARY KEY (id)
-)
+);
+INSERT INTO produkt
+VALUES ('czekoladki','slodycze');
