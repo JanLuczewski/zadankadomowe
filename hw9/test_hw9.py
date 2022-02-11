81f1e07f3be419d95c0fda0b795496bf8ff9687a
JanLuczewski <jan.luczewski@gmail.com>
2022-02-11 10:07:58 +0100

    zadanko 9

--- hw9/test_hw9.py
+++ hw9/test_hw9.py
@@ -7,10 +7,12 @@
 # sprawdzający czy jeśli podam na wejściu stringa to zostanie wyrzucony błąd
 # czy jeśli podam prawidłowe wartości to dostanie poprawną długość trzeciego boku - dwa testy dla różnych wartości
 import pytest
-
-def oblicz_bok(a,b):
-    pass
+import math
+from hw9.triangle import oblicz_bok
 
 def test_oblicz_bok():
-    result = oblicz_bok(3,4)
+    result = oblicz_bok(2,2)
     assert result == 5
+def test_czy_int():
+    with pytest.raises(TypeError) as excinfo:
+        oblicz_bok("6",2)
---
+++ hw9/triangle.py
@@ -0,0 +1,8 @@
+import math
+def oblicz_bok(a,b):
+    #a2 + b2 =c2    (a^2 + b^2)^(1/2)=c
+
+    c = math.sqrt(a**2 + b**2)
+    print(c)
+if __name__ == "__main__":
+    oblicz_bok('6',2)
